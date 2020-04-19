77b65893e3ce1c0398cd3a868f4c305eeb404a03
Jocelyn Wang <jocelynwang@berkeley.edu>
2020-04-18 17:07:37 -0700

    25_1.in using nx.connected_watts_strogatz_graph 25,4,0.8

--- solver.py
+++ solver.py
@@ -17,17 +17,24 @@ def solve(G):
     # TODO: your code here!
 
     #random graph:
-    #G = nx.connected_watts_strogatz_graph(n=25, k=4, p=0.6)
+    #G = nx.connected_watts_strogatz_graph(n=25, k=4, p=0.8)
 
-    # drawing the graph with weights:
-    pos=nx.planar_layout(G)
+    # drawing the graph with weights: spring_layout same length edges / circular_layout easier to see degrees
+    pos=nx.circular_layout(G)
     nx.draw_networkx(G, pos)
-    labels = nx.get_edge_attributes(G,'weight')
+    labels = nx.get_edge_attributes(G,"weight")
     nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
     plt.show()
 
     print([e for e in G.edges])
+
     mst = nx.minimum_spanning_tree(G)
+    #print([i for i in mst.edges])
+
+    # drawing tree:
+    #tree_pos = nx.spring_layout(mst)
+    #nx.draw_networkx(mst, tree_pos)
+    #plt.show()
     return mst
 
     # pass
@@ -38,6 +45,7 @@ def solve(G):
 if __name__ == '__main__':
     assert len(sys.argv) == 2
     path = sys.argv[1]
+
     G = read_input_file(path)
 
     """
@@ -47,7 +55,7 @@ if __name__ == '__main__':
     """
 
     T = solve(G)
-    #assert is_valid_network(G, T)
-    #print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
+    assert is_valid_network(G, T)
+    print("Average  pairwise distance: {}".format(average_pairwise_distance(T)))
     #write_output_file(T, 'out/'+path[:len(path)-3]+'.out')
     #write_output_file(T, 'out/test.out')
