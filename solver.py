168e94c5f04b50b1baa8bc713c8cde67ca152ce2
Jocelyn Wang <jocelynwang@berkeley.edu>
2020-04-21 23:30:07 -0700

    plot stuff

--- solver.py
+++ solver.py
@@ -10,35 +10,40 @@ def solve(G):
     """
     Args:
         G: networkx.Graph
-
     Returns:
         T: networkx.Graph
     """
     # TODO: your code here!
 
     #draw graph:
-    """
+    plt.figure(figsize=(8,8))
     pos=nx.spring_layout(G)
     nx.draw_networkx(G, pos)
     labels = nx.get_edge_attributes(G,"weight")
     nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+
+    #ignore
+    """
+    v_descending_degree = sorted([n for n, d in G.degree()], reverse=True, key=G.degree()) #vertices sorted by degree
+    print(v_descending_degree)
+    max_v = max(v_descending_degree) #vertex with max degree
+    """
+
+    T = nx.minimum_spanning_tree(G)
+
+    #to draw tree in the same plot as G:
+    nx.draw_networkx_edges(G,pos,
+        edgelist=[e for e in T.edges()],
+        width=5,alpha=0.5,edge_color='r')
     plt.show()
-    """
 
-    mst = nx.minimum_spanning_tree(G)
+    #is_valid_network(T) to check if it works
+    #total_pairwise_distance = average_pairwise_distance(T) * (len(T) * (len(T) - 1))
 
-    #draw tree:
-    """
-    tree_pos = nx.spring_layout(mst)
-    nx.draw_networkx(mst, tree_pos)
-    plt.show()
-    """
-
-    return mst
+    return T
 
 # Here's an example of how to run your solver.
-
-# Usage: python3 solver.py test.in
+# Usage: python3 solver.py inputs/small-302.in
 
 if __name__ == '__main__':
     assert len(sys.argv) == 2
