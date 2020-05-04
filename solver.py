36d3278c3d30784039e4b55c74828d1002fd5223
Jocelyn Wang <jocelynwang@berkeley.edu>
2020-05-03 23:33:22 -0700

    final version solver.py

--- solver.py
+++ solver.py
@@ -2,10 +2,9 @@ import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_network, average_pairwise_distance
 import sys
-
 import re
 from networkx.algorithms import tree
-
+from networkx.utils import UnionFind
 from heapq import heappop, heappush
 from itertools import count
 
@@ -26,7 +25,6 @@ def solve(G):
         if (sum_incident_edges <= curr_min_sum):
             curr_max_deg_v = v
             curr_min_sum = sum_incident_edges
-
     max_v = curr_max_deg_v
 
     if len(G.__getitem__(max_v)) == G.number_of_nodes()-1:
@@ -36,7 +34,6 @@ def solve(G):
 
     mst_edges = kruskal_mst_edges(G)
     edgelist = list(mst_edges)
-
     str_edgelist = []
     for edge in edgelist:
         str_edgelist.append(str(str(edge[0])+" "+str(edge[1])+" "+str(edge[2]["weight"])))
@@ -46,7 +43,6 @@ def solve(G):
     v_deg_1 = [v for v in v_ascending_degree if T.degree[v]==1] #all leaves of tree
 
     #looking at all leaves
-
     for v in v_deg_1:
         copy_result = T.copy()
         copy_result.remove_node(v)
@@ -55,65 +51,29 @@ def solve(G):
                 T = copy_result
     return T
 
-
 def kruskal_mst_edges(G, weight='weight', data=True):
     """Generate edges in a minimum spanning forest of an undirected
     weighted graph.
-
-    A minimum spanning tree is a subgraph of the graph (a tree)
-    with the minimum sum of edge weights.  A spanning forest is a
-    union of the spanning trees for each connected component of the graph.
-
     Parameters
     ----------
     G : NetworkX Graph
-
     weight : string
        Edge data key to use for weight (default 'weight').
-
     data : bool, optional
        If True yield the edge data along with the edge.
-
     Returns
     -------
     edges : iterator
        A generator that produces edges in the minimum spanning tree.
        The edges are three-tuples (u,v,w) where w is the weight.
-
-    Examples
-    --------
-    >>> G=nx.cycle_graph(4)
-    >>> G.add_edge(0,3,weight=2) # assign weight 2 to edge 0-3
-    >>> mst=nx.minimum_spanning_edges(G,data=False) # a generator of MST edges
-    >>> edgelist=list(mst) # make a list of the edges
-    >>> print(sorted(edgelist))
-    [(0, 1), (1, 2), (2, 3)]
-
-    Notes
-    -----
-    Uses Kruskal's algorithm.
-
-    If the graph edges do not have a weight attribute a default weight of 1
-    will be used.
-
-    Modified code from David Eppstein, April 2006
-    http://www.ics.uci.edu/~eppstein/PADS/
     """
-    # Modified code from David Eppstein, April 2006
-    # http://www.ics.uci.edu/~eppstein/PADS/
-    # Kruskal's algorithm: sort edges by weight, and add them one at a time.
-    # We use Kruskal's algorithm, first because it is very simple to
-    # implement once UnionFind exists, and second, because the only slow
-    # part (the sort) is sped up by being built in to Python.
-    from networkx.utils import UnionFind
     if G.is_directed():
         raise nx.NetworkXError(
             "Mimimum spanning tree not defined for directed graphs.")
 
-
     subtrees = UnionFind()
+
     #average sum of incident edges
-    
     track_sum = {}
     for edge in G.edges(data=True):
         edge = list(edge)
@@ -123,7 +83,6 @@ def kruskal_mst_edges(G, weight='weight', data=True):
         average_sum_of_incident = sum_of_incident / incident_degree
         edge[2]['weight'] += average_sum_of_incident
         track_sum[str(edge[0]) + str(edge[1]) + str(edge[2]['weight'])] = average_sum_of_incident
-    
     """
     track_sum = {}
     for edge in G.edges(data=True):
@@ -134,24 +93,16 @@ def kruskal_mst_edges(G, weight='weight', data=True):
         edge[2]['weight'] += incident_degree
         track_sum[str(edge[0]) + str(edge[1]) + str(edge[2]['weight'])] = incident_degree
     """
-
-
-
     edges = sorted(G.edges(data=True), key=lambda t: t[2].get(weight, 1))
     for u, v, d in edges:
         if subtrees[u] != subtrees[v]:
             if data:
-
-                
                 remove = track_sum.get(str(u) + str(v) + str(d['weight']))
                 d['weight'] -= remove
-                
-                
                 yield (u, v, d)
             else:
                 yield (u, v)
             subtrees.union(u, v)
-
 
 def prim_mst_edges(G, start):
     """Iterate over edges of Prim's algorithm min/max spanning tree.
@@ -161,6 +112,11 @@ def prim_mst_edges(G, start):
         The graph holding the tree of interest.
     start : int
         The starting vertex to run Prim's on.
+    Returns
+    -------
+    edges : iterator
+        A generator that produces edges in the minimum spanning tree.
+        The edges are three-tuples (u,v,w) where w is the weight.
     """
     push = heappush
     pop = heappop
@@ -177,30 +133,10 @@ def prim_mst_edges(G, start):
         visited = {u}
 
         for v, d in G.adj[u].items(): #all neighbor vertices v, d is weight of (u,v)
-            """#heuristic stuff
-            #sum of v's incident edges (v, w):
-            sum_incident_edges = sum([d2.get("weight") for w, d2 in G.adj[v].items()])
-            deg_v = len(G.__getitem__(v))
-            score = 1
-            print("score of vertex", v, score)
-            """
             wt = d.get("weight") #edge weight
             push(frontier, (wt, next(c), u, v, d))
 
         while frontier:
-            """ #heuristic stuff
-            vertex_v = [v for w, _, u, v, d in frontier]
-
-            #neighborhood of v:
-            neighborhood_v = [set(G.__getitem__(v)) for w, _, u, v, d in frontier]
-            dictionary = dict(zip(vertex_v, neighborhood_v))
-            print(dictionary)
-
-            #non-visited neighbors of v:
-            print(visited.intersection(set(G.__getitem__(v))))
-            #len(G.__getitem__(v)) - len(visited.intersection(set(G.__getitem__(v))))
-            """
-
             W, _, u, v, d = pop(frontier)
             if v in visited or v not in nodes:
                 continue
@@ -230,8 +166,7 @@ if __name__ == '__main__':
             path_string = re.split('[/.]', path)
             write_output_file(T, 'outputs/'+path_string[1]+'.out')
         print(" Total Average Small Pairwise Distance: {}".format(total_pairwise_distance/303))
-        
-        
+
         for i in range(1,304):
             path = 'inputs/medium-'+str(i)+'.in'
             G = read_input_file(path)
@@ -242,8 +177,7 @@ if __name__ == '__main__':
             path_string = re.split('[/.]', path)
             write_output_file(T, 'outputs/'+path_string[1]+'.out')
         print(" Total Average Medium Pairwise Distance: {}".format(total_pairwise_distance/303))
-        
-        
+
         for i in range(1,401):
             path = 'inputs/large-'+str(i)+'.in'
             G = read_input_file(path)
@@ -254,7 +188,6 @@ if __name__ == '__main__':
             path_string = re.split('[/.]', path)
             write_output_file(T, 'outputs/'+path_string[1]+'.out')
         print(" Total Average Large Pairwise Distance: {}".format(total_pairwise_distance/400))
-        
         print(" Total Average Pairwise Distance: {}".format(total_pairwise_distance/1006))
         
     else:
@@ -265,5 +198,3 @@ if __name__ == '__main__':
         print(path + " Average pairwise distance: {}".format(average_pairwise_distance(T)))
         path_string = re.split('[/.]', path)
         write_output_file(T, 'outputs/'+path_string[1]+'.out')
-
-
